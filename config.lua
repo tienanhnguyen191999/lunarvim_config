1760de8aec16531e55682d9b5daac3a2ed6aef2c
Nguyễn Tiến Anh <45494526+tienanhnguyen191999@users.noreply.github.com>
2022-10-22 16:36:06 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -13,9 +13,7 @@ lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.colorscheme = "onedarker"
 -- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
 
--- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
@@ -23,26 +21,14 @@ lvim.keys.insert_mode["jk"] = "<Esc>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 
+lvim.builtin.dap.active = false
 lvim.builtin.treesitter.ensure_installed = "all"
 lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.lsp.diagnostics.virtual_text = false
-lvim.builtin.notify.active = false
 vim.opt.list = true
 vim.opt.listchars = "tab:→/,trail:~,space:·"
 vim.opt.cmdheight = 1
-
--- Change theme settings
-lvim.builtin.theme.options.dim_inactive = true
--- lvim.builtin.theme.options.style = "storm"
-
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
 
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
@@ -65,16 +51,22 @@ linters.setup {
 lvim.plugins = {
   { "iamcco/markdown-preview.nvim", run = "cd app && npm install",
     setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, },
-  { "ggandor/lightspeed.nvim", event = "BufRead" },
+  { "ggandor/lightspeed.nvim" },
   { "tpope/vim-surround" },
   { "tpope/vim-repeat" },
   { "terryma/vim-multiple-cursors" },
-  { "lunarvim/Onedarker.nvim" },
+  { "lunarvim/Onedarker.nvim" }
 }
 
 vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = "highlight WhiteSpace guifg=#3B3E3D guibg=None", })
-vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.php", command = "let @l = \"yoconsole.log('jkpla, jkpA;\"", })
-vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.php", command = "let @l = \"yodd('jkpla, jkpA;\"", })
-vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.php", command = "setlocal ts=4 sw=4", })
-vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.js", command = "setlocal ts=4 sw=4", })
-vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.vue", command = "setlocal ts=2 sw=2", })
+vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.js", command = "let @l = \"yoconsole.log('jkpla, jkpA;jk\"", })
+vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.php", command = "let @l = \"yodd('jkpla, jkpA;jk\"", })
+vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.py", command = "let @l = \"yoprint('jkpla, jkpAjk\"", })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = { "*.php", "*.py" },
+  command = "setlocal ts=4 sw=4", 
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = { "*.vue", "*.js", "*.html", "*.css" },
+  command = "setlocal ts=2 sw=2",
+})
